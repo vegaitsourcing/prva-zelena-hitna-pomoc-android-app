6f8593b648ea00a452b16655f884149e8e69b2e5
stefansavic-vega <s.savic@xximo.com>
2022-10-16 11:40:45 +0200

    fixed bottom bar navigation

--- app/src/main/java/com/example/hakaton/ui/donate/DonateFragment.kt
+++ app/src/main/java/com/example/hakaton/ui/donate/DonateFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.hakaton.databinding.FragmentDonateBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,6 +29,15 @@ class DonateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         loadDonationData()
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        binding.apply {
+            toolbar.toolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 
     private fun loadDonationData() {
